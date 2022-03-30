a05603a4a823397432d7ab2900538f1882bdbfdc
kamseno105 <92008924+kamseno105@users.noreply.github.com>
2022-03-30 11:54:28 +0700

    pwl
    tugas 4

---
+++ phpcrud/index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>WEBSITEKU_KAMSENO-105</h2>
+	<p>Markus Sigit Kamseno 18083000105</p>
+</div>
+
+<?=template_footer()?>
